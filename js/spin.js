efbf45dfd934de7c40af083ec3b249bf45907533
FUNC <qeryip755@126.com>
2023-05-31 13:54:38 +0800

    转移

--- js/spin.js
+++ js/spin.js
@@ -7,46 +7,50 @@ let num = 0
 let preDog = null;
 function move (){
     let dog;
-    // 生成狗子
-     async function createDog(){
-        let newDog = document.createElement('div');
-        newDog.className = 'doge';
-        newDog.id = 'doge'+num;
-        let bg = document.getElementById('doge-bg');
-        /*// 判断当前屏幕是否存在狗子并加入狗子
-           let haveDog = document.querySelectorAll('.doge').length;
-        if(!haveDog){
-            bg.appendChild(newDog);
-        } else {
-            bg.insertBefore(newDog,preDog);
-        }
-        preDog = newDog; */
-        bg.appendChild(newDog);
-        dog = document.getElementById('doge'+num);
-        num++;
-        await getDog(dog);
-    }
-    // 获取当前元素在窗口中的位置并移动
-    function getDog(dog){
-        let rect = dog.getBoundingClientRect();
-        var leftRec = rect.left;
-        let imgLeft = 0;
-        moveController(dog,imgLeft,leftRec);
-    };
     createDog();
 }
 
+// 生成狗子
+function createDog(){
+    let newDog = document.createElement('div');
+    newDog.className = 'doge';
+    newDog.id = 'doge'+num;
+    let bg = document.getElementById('doge-bg');
+    bg.appendChild(newDog);
+    dog = document.getElementById('doge'+num);
+    console.log("🚀 ~ file: spin.js:29 ~ createDog ~ num:", num)
+    getDog(dog);
+    num++;
+}
+
+// 获取当前狗子在窗口中的位置并移动
+function getDog(dog){
+    let rect = dog.getBoundingClientRect();
+    var leftRec = rect.left;
+    let imgLeft = 0;
+    moveController(dog,imgLeft,leftRec);
+};
+
 function moveController(dog,imgLeft,leftRec){
+    // 控制单个狗子的移动
+    let step = 5;
     const moveTimer = setInterval(()=>{
-        if(imgLeft + 250 <= -leftRec) {
+        // 狗子移动完成后执行的动作
+        if(imgLeft + 200 <= -leftRec) {
             dog.remove();
-            clearInterval(moveTimer);            
+            clearInterval(moveTimer);
+            numClear(leftRec,step);
             return;
         }
         transform = 'translateX('+imgLeft+'px)';
         dog.style.transform = transform;
-
-        imgLeft-=5;
-        leftRec--;
+        imgLeft-=step;
     },15);
+}
+
+// 计数归零
+function numClear(leftRec,step){
+    limitNum = Math.floor(leftRec / 50) + step;
+    if(num >= limitNum){ num = 0 }
+    return; 
 }
